d4fa0503916d617d8a32874b2ce4dd6639a808c3
Darrell Payne <darrellpayne@earthlink.net>
2019-11-29 12:20:20 -0800

    41. Sign Up User Without Cloud functions completed
    I didn't actually do 41, as I finished 40 that did
    the same thing with Cloud functions that I considered
    to be a better option. All errors and warnings have
    been fixed up to this point.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.createUserInDatabase = functions.auth.user().onCreate(async user => {
+  const email = user.email;
+
+  try {
+    const snapshot = await admin
+      .database()
+      .ref("users/" + user.uid)
+      .set({ email: email, uid: user.uid });
+    return snapshot;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+});
